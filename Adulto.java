59b8845c6b6e013c260439d2e926c4e975c6211c
Noelgithub-boop <91748397+Noelgithub-boop@users.noreply.github.com>
2022-03-16 19:05:32 +0100

    Update Adulto.java

--- Adulto.java
+++ Adulto.java
@@ -1,15 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package intento4;
 
 /**
  *
- * @author manel
+ * @author ZAMBRANA,Manel y CORTAVARRIA,Noel
  */
 public class Adulto extends Persona{
+    /*private String NISS*/
     private String NISS =new String();
+    /*private String direccion*/
     private String direccion =new String();
     //Constructor
     public Adulto(String nombre, String DNI, Integer edad) {
@@ -19,23 +18,28 @@ public class Adulto extends Persona{
     public Adulto() {
     }
     //getters i setters
+    /*Devuelve el  NISS
+        @return NISS de la persona */
     public String getNISS() {
         return NISS;
     }
-
+    /*Modifica el NISS de la persona
+    @param  NISS*/
     public void setNISS(String NISS) {
         this.NISS = NISS;
     }
-
+    /*Devuelve la direccion de una persona
+    @return direccion*/
     public String getDireccion() {
         return direccion;
     }
-
+    /*Modifica la direccion
+    @param direccion*/
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
     //toString
-
+    /*método toString*/
     @Override
     public String toString() {
         return "Adulto{" + "NISS=" + NISS + ", direccion=" + direccion + '}';
