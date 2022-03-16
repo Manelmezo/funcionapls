fc1d5f9fc77b958b74d14ccadf281192c8e4b0e1
Manelmezo <manelmezo12@gmail.com>
2022-03-16 12:34:57 +0100

    petada de netbeans

---
+++ Petado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intento4;
+
+/**
+ *
+ * @author manel
+ */
+public class Petado {
+    //En Netbeans, al realizar un cambio de ramificación, peta el programa y desaparecen las clases
+}
