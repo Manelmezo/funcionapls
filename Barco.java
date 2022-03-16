c721a945c7aa3928b30b873152eac4bfcd5e199d
Noelgithub-boop <91748397+Noelgithub-boop@users.noreply.github.com>
2022-03-16 19:24:03 +0100

    Update Barco.java

--- Barco.java
+++ Barco.java
@@ -1,52 +1,64 @@
 package intento4;
 
-//hay que importar java.util.date para poder usar la variable date
+/*hay que importar java.util.date para poder usar la variable date*/
 import java.util.Date;
-
+/*Version 5.0.0*/
 /**
  *
- * @author Cortavarria Andara,José Noel
+ * @author Cortavarria Andara,José Noel ,ZAMBRANA ,Manel
  */
 
 public class Barco extends Motor {
+    /*private String nombre*/
     private String nombre;
+    /*Private Date fecha construccion*/
     private Date fechaConstruccion;
+    /*private Integer numCamarotes*/
     private Integer numCamarotes;
-//CONSTRUCTOR
+/*CONSTRUCTOR de la clase Barco*/
     public Barco(String nombre, Date fechaConstruccion, Integer numCamarotes, String fabricante, String potencia, Integer codigo) {
         super(fabricante, potencia, codigo);
         this.nombre = nombre;
         this.fechaConstruccion = fechaConstruccion;
         this.numCamarotes = numCamarotes;
     }
-//GETTERS Y SETTERS
+ /*Devuelve el  nombre
+        @return nombre de la persona */
     
     public String getNombre() {
         return nombre;
     }
-
+ /*Modifica el nombre de la persona
+    @param  nombre*/
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+     /*Devuelve la fecha de la Construccion
+        @return fechaConstruccion */
 
     public Date getFechaConstruccion() {
         return fechaConstruccion;
     }
+     /*Modifica la FechaConstruccion
+    @param  fechaConstruccion*/
 
     public void setFechaConstruccion(Date fechaConstruccion) {
         this.fechaConstruccion = fechaConstruccion;
     }
-
+ /*Devuelve el  numero de camarotes
+        @return numCamarotes */
     public Integer getNumCamarotes() {
         return numCamarotes;
     }
+     /*Modifica el numero de camarotes
+    @param  numCamarotes*/
 
     public void setNumCamarotes(Integer numCamarotes) {
         this.numCamarotes = numCamarotes;
     }
 
 
-//TO STRING
+/*metodo toString*/
 
     @Override
     public String toString() {
