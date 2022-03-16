3ffe60f5a5da0b5cfd2e81426beaa30a78050e79
Noelgithub-boop <91748397+Noelgithub-boop@users.noreply.github.com>
2022-03-16 19:27:22 +0100

    Update Mascota.java

--- Mascota.java
+++ Mascota.java
@@ -1,45 +1,50 @@
 
 package intento4;
-
+/*@Version */
 /**
  *
- * @author Cortavarria Andara,José Noel
+ * @author Cortavarria Andara,José Noel ,ZAMBRANA ,manel
  */
-//IMPLEMENTAMOS LA INTERFAZ SONIDO EN MASCOTA
+/*IMPLEMENTAMOS LA INTERFAZ SONIDO EN MASCOTA*/
 public abstract class Mascota implements Sonido {
     //Atributos
     private String nombre;
     private Integer edad;
     //constructor
 
-
+/*Constructor publico de Mascota */
 
     public Mascota(String nombre, Integer edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
     
-//    getters y setters
+
     public Mascota(){
          }
-
+/*Devuelve el  nombre de la mascota
+        @return nombre */
     public String getNombre() {
         return nombre;
     }
-
+/*Modifica el nombre de la mascota
+    @param  nombre*/
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+/*Devuelve la edad de la mascota
+        @return edad */
     public Integer getEdad() {
         return edad;
     }
+/*Modifica la edad de la mascota
+    @param  edad*/
 
     public void setEdad(Integer edad) {
         this.edad = edad;
     }
     
-    //to String
+    /*to String*/
 
     @Override
     public String toString() {
