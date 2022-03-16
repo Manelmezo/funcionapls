cce0aa7d3284f49cf14b9af3e7e4b4de3e5b3250
Noelgithub-boop <91748397+Noelgithub-boop@users.noreply.github.com>
2022-03-16 19:18:56 +0100

    Update Sonido.java

--- Sonido.java
+++ Sonido.java
@@ -1,13 +1,13 @@
 
 package intento4;
-
+/*Version 5.0.0*/
 /**
  *
  * @author Cortavarria Andara,José Noel
  */
 //Interfaz sonido
 public interface Sonido {
-    //metodo de sonido 
+    /*Método sonido*/
     public void sonido();
     
 }
